da6421939360f6300272f69f1031af8332843793
dominickingston2003 <31516170+dominickingston2003@users.noreply.github.com>
2017-09-01 22:04:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,22 @@
 console.log('Loaded!');
+var commentInput =document.getElementById('comment')
+var comment = commentInput.value
+var submit = document.getElementById('submit_btn')
+submit.onclick = function() {
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE){
+          if(request.status === 200){
+              var names = ['name1','name2','name3','name4'];
+              var list='';
+              for(var i=0;i<name.length;i++){
+                  list += '<li>' + name[i] + '</li>';
+              }
+              var ul = document.getElementById('show')
+              ul.innerHTML = list;
+          }
+      }
+  }
+  request.open('GET','http;//http://dominickingston2003.imad.hasura-app.io/submit-name?name=' + name,true);
+  request.send(null);
+};
