d70f2f5077af856ce540820f1f30136456db0c0f
dominickingston2003 <31516170+dominickingston2003@users.noreply.github.com>
2017-09-01 22:38:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,6 +28,9 @@ app.get('/article-one', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/index.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
